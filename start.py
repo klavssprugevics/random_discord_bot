04c8b68af113f5275bac571cafc1aab50e8d565a
unknown <klavs.sprugevics@gmail.com>
2021-04-30 14:19:19 +0300

    Changed get_secret() to a universal json value finder. Created key-image dictionary support for easy adding of new images. Added on_message() function to display images.

--- start.py
+++ start.py
@@ -1,12 +1,32 @@
 import discord
 
-from utils import get_secret
+from utils import get_value_from_json, open_json
 
 
 # Read secrets
-client_secret = get_secret("ClientSecret")
-
-
+client_secret = get_value_from_json('ClientSecret', './secrets.json')
 client = discord.Client()
-client.run(client_secret)
 
+# Read image dictionary
+image_dictionary = open_json('./image_dictionary.json')
+
+# Events
+@client.event
+async def on_ready():
+    print('Bot successfully logged in as: {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    # Checks if user has typed one of the keywords in the image dictionary
+    for key in image_dictionary:    
+        if(message.content == key):
+            with open(image_dictionary[key], 'rb') as image:
+                await message.channel.send(file=discord.File(image))
+                break
+    
+
+
+
+# Startup
+client.run(client_secret)
--- utils.py
+++ utils.py
@@ -1,10 +1,18 @@
 import json
-    
-def get_secret(key):
-    with open('secrets.json') as secrets_file:
-        secrets = json.load(secrets_file)
+
+# Reads a json file and returns the value from a given key
+def get_value_from_json(key, filename):
+    with open(filename) as file:
+        dictionary = json.load(file)
 
         try:
-            return secrets[key]
+            return dictionary[key]
         except KeyError:
-            raise Exception("Couldn't find secret with given key.")
+            raise Exception('Could not find value with the given key.')
+
+
+# Reads and returns a json file
+def open_json(filename):
+    with open(filename) as dictionary:
+        json_dict = json.load(dictionary)
+        return json_dict
