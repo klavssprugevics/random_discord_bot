53f6e5fed343384c01cf1faa8d3d6bb82f8966fd
unknown <klavs.sprugevics@gmail.com>
2021-05-05 16:33:39 +0300

    Created simple coinflip function

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import discord
+import random
 
 from utils import get_value_from_json, open_json
 
@@ -18,13 +19,22 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
     # Checks if user has typed one of the keywords in the image dictionary
     for key in image_dictionary:    
-        if(message.content == key):
+        if(message.content.upper() == key):
             with open(image_dictionary[key], 'rb') as image:
                 await message.channel.send(file=discord.File(image))
                 break
     
+    # Coinflip simulation
+    if(message.content.upper() == '!COINFLIP'):
+        if(random.random() < 0.5):
+            with open('images/coinflip/heads.png', 'rb') as image:
+                await message.channel.send(file=discord.File(image), content='Heads!')
+        else:
+            with open('images/coinflip/tails.png', 'rb') as image:
+                await message.channel.send(file=discord.File(image), content='Tails!')
 
 
 
